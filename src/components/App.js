a58baac8237bcc7cee5dd7e9806ec26726340d80
Saurabh <saurabhgawande2016@gmail.com>
2023-01-08 23:27:28 +0530

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,26 @@ import '../styles/App.css';
 
 class App extends React.Component {
   
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: "John Doe",
+             enrollmentNo: "12345678",
+              age: 34
+        };
+      }
+    
+      render() {
+        return (
+          <>
+            <h1>Hello, my name is {this.state.name}</h1>
+             <p>I am {this.state.age} years old and my enrollment no is {this.state.enrollmentNo}</p>
+            <button onClick={()=>{this.setState({age: this.state.age+1})}}>
+              Increase age
+            </button>
+          </>
+        );
+      }
 }
-
 
 export default App;
